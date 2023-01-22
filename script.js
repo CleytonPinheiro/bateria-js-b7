2450e7edeb8964dc968ea9032cc5103fa91edcc7
Cleyton <42612444+CleytonPinheiro@users.noreply.github.com>
2023-01-21 22:30:23 -0300

    implementando pra tocar com clique

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+document.body.addEventListener('click', (e) => {
+    let boxes = e.target.getAttribute('data-key');
+
+    playSound(boxes);
+});
+
 document.body.addEventListener('keyup', (event) => {
     playSound(event.code.toLocaleLowerCase());
 });
 
 document.querySelector('.composer button').addEventListener('click', () => {
     let song = document.querySelector('#input').value;
-
     
     if(song !== '') {
         let songArray = song.split('');
 
-       playComposition(songArray);
+        playComposition(songArray);
     }
 });
 
@@ -30,7 +35,7 @@ function playSound(sound) {
     let keyElement = document.querySelector(`div[data-key="${sound}"]`);
 
     if(audioElement) {
-        audioElement.currentTime
+        audioElement.currentTime;
         audioElement.play();
     }
 
